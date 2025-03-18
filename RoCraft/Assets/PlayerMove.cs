e044fabc7bf6a4c89e0c3ffac3c270841cfba95f
therealdatmoose995 <dattmoose995@gmail.com>
2025-03-18 16:41:45 +1300

    Fixed Player moving too fast diagonally

--- RoCraft/Assets/PlayerMove.cs
+++ RoCraft/Assets/PlayerMove.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
@@ -18,7 +19,7 @@ public class PlayerMove : MonoBehaviour
     void FixedUpdate()
     {
         //Movement
-        rb.MovePosition(rb.position + Movement * movespeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + Movement.normalized * movespeed * Time.fixedDeltaTime);
 
     }
 }
