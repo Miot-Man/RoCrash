e2f2114b04d70624569d277eb58adcb93e4454ac
Miot-Man <dattmoose995@gmail.com>
2025-03-19 22:20:18 +1300

    added player Healthbar

--- RoCrash/Assets/Scripts/PlayerManager.cs
+++ RoCrash/Assets/Scripts/PlayerManager.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour, IDamageable
 {
     public float health = 10f;
+    public healthbar healthbar;
     
     void Start()
     {
         GameManager.player = gameObject;
+        healthbar.setmaxhealth(health);
     }
 
     // Update is called once per frame
@@ -19,6 +21,7 @@ public class PlayerManager : MonoBehaviour, IDamageable
     void IDamageable.damage(float damage)
     {
         health -= damage;
+        healthbar.sethealth(health);
         Debug.Log("Player Took Damage");
         if (health <= 0)
         {
