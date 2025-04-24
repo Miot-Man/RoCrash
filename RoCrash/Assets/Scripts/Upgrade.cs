eef4d22727acec424280501e39baa355993903e9
Miot-Man <dattmoose995@gmail.com>
2025-04-24 12:36:26 +1200

    began fixing code

---
+++ RoCrash/Assets/Scripts/Upgrade.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgrade : MonoBehaviour
+{
+    public string upgradeType;
+    
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("straight upgrading");
+            switch (upgradeType){
+                case "Damage":
+                    GameManager.damagemult *= 1.5f;
+                    break;
+                case "Health":
+                    GameManager.healthmult += 2f;
+                    GameManager.player.GetComponent<PlayerManager>().IncreaseHealth();
+                    break;
+                //case "Healthpack":
+                //GameManager.player.GetComponent<PlayerManager>().heal();
+                //break;
+            }
+            Destroy(gameObject);
+        }
+        
+    }
+
+
+}
